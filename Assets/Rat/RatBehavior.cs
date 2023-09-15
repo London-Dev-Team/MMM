67cfb5e9ee5ea6b7e59616b5a9532084dee2d021
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2023-09-15 15:45:55 -0400

    Cleaning

--- Assets/Rat/RatBehavior.cs
+++ Assets/Rat/RatBehavior.cs
@@ -8,7 +8,7 @@ using Random = UnityEngine.Random;
 public class RatBehavior : MonoBehaviour
 {
 
-    private enum MovementState { Moving, Stopped };
+    public enum MovementState { Moving, Stopped, OnFlywheel };
     private MovementState movementState = MovementState.Moving;
     private enum DirMoving { Right = 1, Left = -1};
     private DirMoving dirMoving = DirMoving.Right;
@@ -35,60 +35,111 @@ public class RatBehavior : MonoBehaviour
     private float stopTimeTarget;
     private float stopTime = 0.0f;
     
-    void Start()
+    private void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        boxCollider2D = GetComponent<BoxCollider2D>();
         rb2D.velocity = Vector3.right * speed;
+        boxCollider2D = GetComponent<BoxCollider2D>();
         SetNewMoveTimeTarget();
         SetNewStopTimeTarget();
     }
 
     private void Update()
     {
-        Debug.Log(movementState);
+        if (movementState == MovementState.OnFlywheel){
+            return;
+        }
+        
+        UpdateMoveTimer();
+        UpdateStopTimer();
+    }
+
+    
+    private void UpdateMoveTimer()
+    {
         if (movementState == MovementState.Moving){
             moveTime += Time.deltaTime;
         } 
-        else if (movementState == MovementState.Stopped){
-            stopTime += Time.deltaTime;
-        }
-
         if (moveTime > moveTimeTarget){
-            moveTime = 0.0f;
-            movementState = MovementState.Stopped;
-            rb2D.velocity = new Vector2( 0.0f, rb2D.velocity.y);
-            SetNewMoveTimeTarget();
-        }
-
-        if (stopTime > stopTimeTarget){
-            stopTime = 0.0f;
-            movementState = MovementState.Moving;
-            SetNewStopTimeTarget();
+            SetState(MovementState.Stopped);
         }
     }
 
-    void SetNewMoveTimeTarget()
+    private void UpdateStopTimer()
+    {
+        if (movementState == MovementState.Stopped){
+            stopTime += Time.deltaTime;
+        }
+        if (stopTime > stopTimeTarget){
+            SetState(MovementState.Moving);
+        }
+    }
+
+    private void SetNewMoveTimeTarget()
     {
         moveTimeTarget = Random.Range(minMoveTime, maxMoveTime);
     }
     
-    void SetNewStopTimeTarget()
+    private void SetNewStopTimeTarget()
     {
         stopTimeTarget = Random.Range(minStopTime, maxStopTime);
     }
     
-    void FixedUpdate()
+    
+    public void SetState(MovementState newState)
     {
-        if (movementState == MovementState.Moving){
-            rb2D.velocity = new Vector2( (int)dirMoving * speed, rb2D.velocity.y);
-        
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2( Math.Sign(rb2D.velocity.x) * 1.0f , 0.0f), 0.6f);
-
-            if (hit.collider != null)
-            {
-                dirMoving = (DirMoving)(-1 * (int) dirMoving);
-            }
+        switch(newState) 
+        {
+            case MovementState.Moving:
+                SetMovingState();
+                break;
+            case MovementState.Stopped:
+                SetStoppedState();
+                break;
+            case MovementState.OnFlywheel:
+                SetOnFlywheelState();
+                break;
+            default:
+                Debug.LogError("Unknown MovementState passed");
+                break;
         }
+    }
+    
+    private void SetMovingState()
+    {
+        stopTime = 0.0f;
+        movementState = MovementState.Moving;
+        SetNewStopTimeTarget();
+    }
+    
+    private void SetStoppedState()
+    {
+        movementState = MovementState.Stopped; 
+        moveTime = 0.0f;
+        rb2D.velocity = new Vector2( 0.0f, rb2D.velocity.y);
+        SetNewMoveTimeTarget();
+    }
+    
+    private void SetOnFlywheelState()
+    {
+        movementState = MovementState.OnFlywheel; 
+        rb2D.velocity = new Vector2( 0.0f, rb2D.velocity.y);
+    }
+    
+    private void FixedUpdate()
+    {
+        if (movementState != MovementState.Moving){
+            return;
+        }
+        
+        rb2D.velocity = new Vector2( (int)dirMoving * speed, rb2D.velocity.y);
+    
+        var hit = Physics2D.Raycast(transform.position, new Vector2( Math.Sign(rb2D.velocity.x) * 1.0f , 0.0f), 0.6f);
+
+        if (hit.collider != null)
+        {
+            dirMoving = (DirMoving)(-1 * (int) dirMoving);
+        }
+        
     }
 }
