329d0fd66f462a361e86cd67e7fbd20b64008197
Aiden Imbeau <aidenimbeau12@gmail.com>
2023-09-27 10:25:45 -0400

    Fixed Boolean
    It was late at night, apologies.

--- Assets/RBGravityBehaviour.cs
+++ Assets/RBGravityBehaviour.cs
@@ -7,27 +7,27 @@ public class RBGravityBehaviour : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    
+    [SerializeField] float velocityThreshold = 1f;
 
     [SerializeField] float gravityScale = 3.5f;
     [SerializeField] float fallGravityScale = 6.75f;
 
-    [SerializeField] float velocityThreshold = 5f;
-
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
-        rb.gravityScale = gravityScale;
-        if (!(rb.velocity.y < 1 && rb.velocity.y > -1))
+        if (rb.velocity.y < -(velocityThreshold))
         {
             rb.gravityScale = fallGravityScale;
+        }
+        else
+        {
+            rb.gravityScale = gravityScale;
         }
 
     }
